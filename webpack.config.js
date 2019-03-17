746014e91577b4a9f9b045886602f1600ab20114
Pablo Frias <pafrias90@gmail.com>
2019-03-17 11:37:58 -0700

    Set up babel and webpack

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+// import path from 'path';
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const combineLoaders = require('webpack-combine-loaders');
+const path = require('path');
+
+
+module.exports = {
+  entry: path.resolve(__dirname, './client/src/App.jsx'),
+  output: {
+    path: path.resolve(__dirname, './client/dist'),
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        loader: combineLoaders([
+          {
+            loader: 'style-loader',
+          }, {
+            loader: 'css-loader',
+            query: {
+              modules: true,
+              localIdentName: '[name]__[local]___[hash:base64:5]',
+            },
+          },
+        ]),
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './client/dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+    // new HtmlWebpackPlugin({ template: './src/index.html' }),
+  ],
+};
